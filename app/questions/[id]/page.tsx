abb4d1d705985dc91b3aed7da8c7b46fdd3b72c2
itc-s24013 <s24013@std.it-college.ac.jp>
2025-10-17 15:39:36 +0900

    パスワードを比較する処理を追加

--- app/questions/[id]/page.tsx
+++ app/questions/[id]/page.tsx
@@ -1,4 +1,7 @@
+"use client"
+
 import styles from "./page.module.css"
+import {useState} from "react";
 
 export default function Question(id: number) {
     type questionData = {
@@ -33,24 +36,43 @@ export default function Question(id: number) {
         }
     ]
 
+    const [inputPass, setInputPass] = useState('');
+
+    // 入力値が変更されるたびに実行されるハンドラ
+    const handleChange = (event:React.ChangeEvent<HTMLInputElement>) => {
+        // Stateを新しい入力値で更新
+        setInputPass(event.target.value);
+    };
+
     const questions = JSON.parse(JSON.stringify(questionsData));
 
     questions.map((question: questionData) => {
         if (question.id === id) {
+            const handleSubmit = () => {
+                if (question.password === inputPass) {
+                    return (
+                        <>
+                            <main>
+                                <div className={styles.question}>
+                                    <h1 className="text-2xl font-bold mb-6 bg-gray-200 p-2 rounded">問題を選択してください</h1>
+                                    <h1>{question.questionDescription.title}</h1>
+                                    <p>{question.questionDescription.content}</p>
+                                </div>
+                            </main>
+                        </>
+                    )
+                }
+            };
+            handleSubmit();
+        }
+    }
+    )
+
             return (
                 <>
-                    <main>
-                        <div className={styles.question}>
-                            <h1 className="text-2xl font-bold mb-6 bg-gray-200 p-2 rounded">問題を選択してください</h1>
-                            <h1>{question.questionDescription.title}</h1>
-                            <p>{question.questionDescription.content}</p>
-                        </div>
-                    </main>
+                    <div className={styles.question}>
+                        <h1>パスワードが違います</h1>
+                    </div>
                 </>
             )
-        }
-    })
-    return(
-        <h1 className="text-2xl font-bold mb-6 bg-gray-200 p-2 rounded">問題を選択してください</h1>
-    )
-}
+    }
