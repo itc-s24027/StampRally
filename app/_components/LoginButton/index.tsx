c158754f5d47b4ecc093101a99352e641597c8ec
itc-s24013 <s24013@std.it-college.ac.jp>
2025-10-17 13:38:01 +0900

    LoginButtonのデザインを設定

--- app/_components/LoginButton/index.tsx
+++ app/_components/LoginButton/index.tsx
@@ -2,13 +2,14 @@
 
 import {signIn} from "next-auth/react";
 import Image from "next/image";
+import styles from "./index.module.css"
 
 export default function Index() {
 
     return (
         <>
-            <button onClick={() => signIn("google" , {callbackUrl: "http://localhost:3000/questions"})}>
-                <Image src="/images/web_neutral_sq_SI@3x.png" alt="" width={250} height={60} />
+            <button className={styles.loginButton} onClick={() => signIn("google" , {callbackUrl: "http://localhost:3000/questions"})}>
+                <Image src="/images/web_light_sq_SI@3x.png" alt="" width={567} height={120} />
             </button>
         </>
     );
