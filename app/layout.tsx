fb974d943d312f3a1a32a4759e023eb19c94c227
itc-s24013 <s24013@std.it-college.ac.jp>
2025-10-21 21:18:19 +0900

    Headerを全ページに適用

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import NextAuthProvider from "@/app/providers/SessionProvider";
 import "./globals.css";
+import Header from "@/app/_components/Header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,6 +26,7 @@ export default function RootLayout({ children }: Readonly<{
     <html lang="ja">
         {/*geistSans.variable = デフォルトのフォント*/}
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <Header />
         <NextAuthProvider>
             {children}
         </NextAuthProvider>
