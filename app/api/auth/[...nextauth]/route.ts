d7ec5f332e0fe4711a12251d22dc0f00de0debef
itc-s24016 <s24016@std.it-college.ac.jp>
2025-10-27 18:24:54 +0900

    名前とメアドの取得のみ

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,15 +1,13 @@
-import NextAuth, { NextAuthOptions, User, Account, Profile } from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import { prismaClient } from "@/lib/prismaClient";
-// import { AdapterUser } from "next-auth/adapters";
-// import { User } from "@/lib/types";
+import NextAuth, { NextAuthOptions, User, Account, Profile } from "next-auth";// 認証を管理
+import GoogleProvider from "next-auth/providers/google";// 「Googleアカウント」のログイン機能
+import { PrismaAdapter } from "@auth/prisma-adapter";// NextAuth と Prisma を接続する
+import { prismaClient } from "@/lib/prismaClient"; // Prisma で DB 接続
 
 export const authOptions: NextAuthOptions = {
     adapter: PrismaAdapter(prismaClient), // NextAuthとPrismaをつなぐアダプター
     session: { strategy: "jwt" as const }, // JWT = JSON形式のデータを安全にやり取りするための仕組み
     secret: process.env.NEXTAUTH_SECRET, // セッションの暗号化
-    providers: [ // ログイン方法
+    providers: [ // ログイン方法（今回は Google のみ）
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID!,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
@@ -29,13 +27,13 @@ export const authOptions: NextAuthOptions = {
             // サインインしたユーザーが学生テーブルに存在しなければ処理を中断する
             if (!user?.id) return;
 
-            const existing = await prismaClient.student.findUnique({
-                where: { userId: user.id }
+            const existing = await prismaClient.user.findUnique({
+                where: { id: user.id }
             })
             if (!existing) {
-                await prismaClient.student.create({
+                await prismaClient.user.create({
                     data: {
-                        userId: user.id,
+                        id: user.id,
                         name: user.name ?? "",
                         email: user.email ?? "",
                     },
