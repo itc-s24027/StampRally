b4007e58ab808ae9a3b18e64016710537ff7a951
itc-s24013 <s24013@std.it-college.ac.jp>
2025-10-30 19:38:36 +0900

    ちょっとしたレスポンシブを作成

--- app/_components/Correct/index.tsx
+++ app/_components/Correct/index.tsx
@@ -9,7 +9,7 @@ export default function Correct() {
         <div className="text-center">
             <br/>
             <h1>正解！</h1>
-            <Image src="/images/mark_maru.png" alt="" width={540} height={540} />
+            <Image src="/images/mark_maru.png" alt="" width={540} height={540} className="w-25 h-25" />
             <br />
             <button className="btn btn-outline-secondary mx-2" onClick={() => router.push('http://localhost:3000/questions')}>
                 一覧へ
--- app/_components/Incorrect/index.tsx
+++ app/_components/Incorrect/index.tsx
@@ -13,7 +13,7 @@ export default function Correct({id}: Props) {
         <div className="text-center">
             <br/>
             <h1 className="fw-bold">不正解！</h1>
-            <Image src="/images/mark_batsu.png" alt="" width={540} height={540} />
+            <Image src="/images/mark_batsu.png" alt="" width={540} height={540} className="w-25 h-25"/>
             <br />
             <button className="btn btn-outline-secondary mx-2" onClick={() => router.push('http://localhost:3000/questions')}>
                 一覧へ
