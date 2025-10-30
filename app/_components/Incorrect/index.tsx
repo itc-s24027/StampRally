278db48561b5d84d7bd34dc7f7550510a97e7271
itc-s24013 <s24013@std.it-college.ac.jp>
2025-10-30 19:18:27 +0900

    不正解ページ作成

---
+++ app/_components/Incorrect/index.tsx
@@ -0,0 +1,26 @@
+"use client"
+
+import Image from "next/image";
+import { useRouter } from 'next/navigation'
+
+type Props = {
+    id: number;
+}
+
+export default function Correct({id}: Props) {
+    const router = useRouter()
+    return (
+        <div className="text-center">
+            <br/>
+            <h1 className="fw-bold">不正解！</h1>
+            <Image src="/images/mark_batsu.png" alt="" width={540} height={540} />
+            <br />
+            <button className="btn btn-outline-secondary mx-2" onClick={() => router.push('http://localhost:3000/questions')}>
+                一覧へ
+            </button>
+            <button className="btn btn-outline-primary" onClick={() => router.push(`http://localhost:3000/questions/${id}`)}>
+                再挑戦
+            </button>
+        </div>
+    )
+}
---
+++ app/questions/incorrect/page.tsx
@@ -0,0 +1,7 @@
+import InCorrect from "@/app/_components/Incorrect";
+
+export default function Page() {
+    return (
+        <InCorrect id={1} />
+    )
+}
