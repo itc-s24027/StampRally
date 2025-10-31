e9b33e65751da10993482a47544ee369dc9a53e9
itc-s24025 <s24025@std.it-college.ac.jp>
2025-10-31 23:37:22 +0900

    問題数を6問にした。スキーマにfloorを追加。

--- app/api/questionRegistry/route.ts
+++ app/api/questionRegistry/route.ts
@@ -17,11 +17,7 @@ export async function POST() {
             {question_text: "学内ハロウィンイベントでいまり先生は何の仮装をしていたでしょう？", correct_answer: "1", password: "question5",
                 option_a: "1. 魔女", option_b: "2. キョンシー", option_c: "3. メイドさん", option_d: "4. 雛子(SILENT HILL f)" },
             {question_text: "学内ハロウィンイベントで大桝先生は何の仮装をしていたでしょう？", correct_answer: "3", password: "question6",
-                option_a: "1. 牧師", option_b: "2. 吸血鬼", option_c: "3. 黒執事", option_d: "4. 名探偵コナン" },
-            { question_text: "いまり先生は何カ国行ったことがあるでしょう？", correct_answer: "2", password: "question7",
-                option_a: "1. １カ国", option_b: "2. ２カ国", option_c: "3. ３カ国", option_d: "4. ４カ国" },
-            { question_text: "このプログラムの実行結果はどれでしょう？\na = [1, 2, 3]\nb = a\nb.append(4)\nprint(a)", correct_answer: "1", password: "question8",
-                option_a: "1. [1, 2, 3, 4]", option_b: "2. [1, 2, 3]", option_c: "3. a", option_d: "4. Error" }]);
+                option_a: "1. 牧師", option_b: "2. 吸血鬼", option_c: "3. 黒執事", option_d: "4. 名探偵コナン" }]);
 
     if (error) {
         console.error(error);
