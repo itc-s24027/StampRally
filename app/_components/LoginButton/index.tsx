c05b6a5cf6f6a9036a62caafde7bc32d038030eb
itc-s24013 <s24013@std.it-college.ac.jp>
2025-10-31 12:10:44 +0900

    BootStrapを適用

--- app/_components/LoginButton/index.tsx
+++ app/_components/LoginButton/index.tsx
@@ -2,14 +2,13 @@
 
 import {signIn} from "next-auth/react";
 import Image from "next/image";
-import styles from "./index.module.css"
 
 export default function Index() {
 
     return (
         <>
-            <button className={styles.loginButton} onClick={() => signIn("google" , {callbackUrl: "http://localhost:3000/questions"})}>
-                <Image src="/images/web_light_sq_SI@3x.png" alt="" width={567} height={120} />
+            <button className="btn border-0 position-absolute top-50 start-50 translate-middle" onClick={() => signIn("google" , {callbackUrl: "http://localhost:3000/questions"})}>
+                <Image src="/images/web_light_sq_SI@3x.png" alt="" width={567} height={120} className="w-100 h-100" />
             </button>
         </>
     );
